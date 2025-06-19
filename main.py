2ad20d276a07175cd9ca399fb20bcd796cc0f56c
Sandra Vorkapic <ilic.z.sandra@gmail.com>
2025-06-19 12:16:22 +0200

    Izmenjena imena/količine proizvoda u main.py; uklonjen prikaz proizvoda i ukupne vrednosti

--- main.py
+++ main.py
@@ -6,19 +6,13 @@ from product_manager import ProductManager
 # Kreiranje instance ProductManager
 manager = ProductManager()
 
-# Dodavanje nekoliko proizvoda
-p1 = Product("Laptop", 85000, 2)
-p2 = Product("Miš", 1500, 10)
-p3 = Product("Tastatura", 3000, 5)
-p4 = Product("Monitor", 25000, 3)
+p1 = Product("Tablet", 45000, 3)
+p2 = Product("Slušalice", 2500, 8)
+p3 = Product("Web kamera", 5000, 6)
+p4 = Product("Punjač", 2000, 7)
+
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
 manager.add_product(p4)
-
-# Prikaz svih proizvoda
-manager.display_all_products()
-
-# Prikaz ukupne vrednosti inventara
-manager.total_value()
